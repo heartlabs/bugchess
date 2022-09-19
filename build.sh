5dcf562aed75a6f9df3067c0ed6279de257ed304
heartlabs <neidhart.orlich@prima.it>
2022-09-19 13:24:09 +0200

    update wasm-bindgen

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ mkdir -p html
 wasm-bindgen target/wasm32-unknown-unknown/release/megachess.wasm --out-dir html --target web --no-typescript
 
 # Shim to tie the thing together
-sed -i "s/import \* as __wbg_star0 from 'env';//" html/megachess.js
-sed -i "s/let wasm;/let wasm; export const set_wasm = (w) => wasm = w;/" html/megachess.js
-sed -i "s/imports\['env'\] = __wbg_star0;/return imports.wbg\;/" html/megachess.js
+sed -i.bak "s/import \* as __wbg_star0 from 'env';//" html/megachess.js
+sed -i.bak "s/let wasm;/let wasm; export const set_wasm = (w) => wasm = w;/" html/megachess.js
+sed -i.bak "s/imports\['env'\] = __wbg_star0;/return imports.wbg\;/" html/megachess.js
+sed -i.bak "s/const imports = getImports();/return getImports();/" html/megachess.js
